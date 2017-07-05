ff19ff7dd701c8625d4fe6c13a1fe2b925e8b4d9
Roy <roybleas@ozemail.com.au>
2017-07-05 11:11:19 +1000

    Refactor controller to just use latest or current year

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -8,6 +8,8 @@ class HomePageController < ApplicationController
   end
 
   def load
+    @this_year = PeopleSecretsantas.maximum('year')
+    @this_year = Date.today.year if @this_year.nil?
   end
 
   def import
--- spec/features/load_pages_spec.rb
+++ spec/features/load_pages_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "HomePages", type: :feature do
-  scenario "Shows the current year" do
+  scenario "Shows default of the current year" do
     visit load_path
 
-    expect(page.has_field?("currentyear", :with => Date.today.year))
-  
+    expect(page).to have_selector("input[value='#{(Date.today.year)}']")
+    expect(page).to have_selector("input[type='number']")
+    expect(page).to have_selector("input[min='#{(Date.today.year)}']")
+
   end
+
+  scenario "Shows default of latest sanata year" do
+    FactoryGirl.create(:people_secretsanta, year: 2013)
+    @person_santa = FactoryGirl.create(:people_secretsanta, year: 2015)
+    FactoryGirl.create(:people_secretsanta, year: 2014)
+
+    visit load_path
+
+    expect(page).to have_selector("input[value='#{(@person_santa.year)}']")
+    expect(page).to have_selector("input[type='number']")
+    expect(page).to have_selector("input[min='#{(@person_santa.year)}']")
+
+  end
+
+
 
 
 end
