260103768707879cb78bc42a1d9aa3ada74f6a56
Roy <roybleas@ozemail.com.au>
2017-07-13 10:24:25 +1000

    make table headings to home view more descriptive

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -16,17 +16,17 @@ RSpec.feature "HomePages", type: :feature do
     visit root_path
 
     expect(current_path).to eq root_path
-    expect(page).to have_css("h4", "Secret Santa participants for #{ps.year}")
 
+    expect(page).to have_css("h4", "Secret Santa participants for #{ps.year}")
     expect(page).to have_css("table")
-    ['Name' , 'Santa', 'Partner', 'Santa from last year' ].each do |th|
-      expect(page).to have_css("th", text: th)
+    [/\AParticipant\z/ , /\ASecret Santa for\z/, /\APartner\z/, /\ASecret Santa from last year\z/ ].each do |header|
+      expect(page).to have_css("th", text: header)
     end
     expect(page).to have_css("tbody tr td", text: p.name)
     expect(page).to have_css("tbody tr td", text: s.name)
     expect(page).to have_css("tbody tr td", text: partner.name)
     expect(page).to have_css("tbody tr td", text: previous.name)
-    
+
   end
 
   scenario "ignores missing santa, partner and previous_santa values" do
