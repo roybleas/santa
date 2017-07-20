5ba714f737ed50e8239bee212c0d7ac3ec047eb1
Roy <roybleas@ozemail.com.au>
2017-07-20 11:38:29 +1000

    Add links to each participant to show person

--- spec/features/people_secretsantas_spec.rb
+++ spec/features/people_secretsantas_spec.rb
@@ -22,23 +22,59 @@ RSpec.feature "PeopleSecretsantas", type: :feature do
     end
   end
 
-  scenario "show participants for selected year" do
-    ps_current = FactoryGirl.create(:people_secretsanta)
-    ps = FactoryGirl.create(:secretsanta_with_santa_partner_previous, year: ps_current.year - 1)
-    p = Person.find(ps.person_id)
-    s = Person.find(ps.santa_id)
-    partner = Person.find(ps.partner_id)
-    previous = Person.find(ps.previous_santa_id)
-
-    visit people_secretsanta_path(ps.year)
-
-    expect(current_path).to eq people_secretsanta_path(ps.year)
-    expect(page).to have_css("h4", text: "Secret Santa participants for #{ps.year}")
-    expect(page).to_not have_css("h4", text: "Current ")
-
-    [p.name, s.name, partner.name, previous.name ].each do |this_name|
-      expect(page).to have_content(this_name)
+  context "show" do
+    before(:context) do
+      DatabaseCleaner.start
+      ps_current = FactoryGirl.create(:people_secretsanta)
+      @ps = FactoryGirl.create(:secretsanta_with_santa_partner_previous, year: ps_current.year - 1)
+      @p = Person.find(@ps.person_id)
+      @s = Person.find(@ps.santa_id)
+      @partner = Person.find(@ps.partner_id)
+      @previous = Person.find(@ps.previous_santa_id)
     end
+    after(:context) { DatabaseCleaner.clean}
+
+    scenario "participants for selected year" do
+      visit people_secretsanta_path(@ps.year)
+
+      expect(current_path).to eq people_secretsanta_path(@ps.year)
+      expect(page).to have_css("h4", text: "Secret Santa participants for #{@ps.year}")
+      expect(page).to_not have_css("h4", text: "Current ")
+      [@p.name, @s.name, @partner.name, @previous.name ].each do |this_name|
+        expect(page).to have_content(this_name)
+      end
+    end
+
+    scenario "creates link to person" do
+      visit people_secretsanta_path(@ps.year)
+      expect(current_path).to eq people_secretsanta_path(@ps.year)
+      click_link(@p.name)
+      expect(current_path).to eq person_path(@p.id)
+    end
+    scenario "creates link to santa" do
+      visit people_secretsanta_path(@ps.year)
+      click_link(@s.name)
+      expect(current_path).to eq person_path(@s.id)
+    end
+    scenario "creates link to partner" do
+      visit people_secretsanta_path(@ps.year)
+      click_link(@partner.name)
+      expect(current_path).to eq person_path(@partner.id)
+    end
+    scenario "creates link to previous santa" do
+      visit people_secretsanta_path(@ps.year)
+      click_link(@previous.name)
+      expect(current_path).to eq person_path(@previous.id)
+    end
+
+    scenario "ignore links when people ids are null" do
+      ps2 = FactoryGirl.create(:archived_secretsanta)
+      p2 = Person.find(ps2.person_id)
+      visit people_secretsanta_path(@ps.year)
+      expect(current_path).to eq people_secretsanta_path(@ps.year)
+      expect(page).to have_link(p2.name)
+    end
+
   end
 
   scenario "delete participants list for selected year" do
@@ -62,9 +98,7 @@ RSpec.feature "PeopleSecretsantas", type: :feature do
     ps2 = FactoryGirl.create(:archived_secretsanta)
     ps_older = FactoryGirl.create(:people_secretsanta, year: ps.year - 1, person_id:  ps2.person_id )
     people_count = Person.count
-
     this_year = ps.year
-
 
     visit archives_path
     click_link(this_year)
