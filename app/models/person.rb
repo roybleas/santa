0d2b56c5bf70f7aea4a25dfd4accf28f7865c5c4
Roy <roybleas@ozemail.com.au>
2017-07-22 13:50:26 +1000

    tidy models

--- app/models/person.rb
+++ app/models/person.rb
@@ -20,14 +20,20 @@ class Person < ActiveRecord::Base
   has_many :partners, :class_name => 'PeopleSecretsantas', :foreign_key => 'partner_id'
   has_many :previous_santas, :class_name => 'PeopleSecretsantas', :foreign_key => 'previous_santa_id'
 
-  scope :include_people_secretsantas, -> {includes(:people_secretsantas).references(:people_secretsantas)}
+  scope :include_people_secretsantas, -> {includes(:people_secretsantas).
+    references(:people_secretsantas)}
   scope :include_santas, -> {includes(:santas).references(:santas)}
   scope :include_partners, -> {includes(:partners).references(:partners)}
   scope :include_previous_santas, -> {includes(:previous_santas).references(:previous_santas)}
 
   scope :non_participants, -> {
     select("people.id ").
-    joins("left join people_secretsantas on people.id = people_secretsantas.person_id left join people_secretsantas partner on people.id = partner.partner_id left join people_secretsantas santa on people.id = santa.santa_id left join people_secretsantas previous on people.id = previous.previous_santa_id").
-    where("people_secretsantas.person_id is null and partner.partner_id is null and santa.santa_id is null and previous.previous_santa_id is null ")
+    joins("left join people_secretsantas on people.id = people_secretsantas.person_id" \
+      " left join people_secretsantas partner on people.id = partner.partner_id" \
+      " left join people_secretsantas santa on people.id = santa.santa_id" \
+      " left join people_secretsantas previous on people.id = previous.previous_santa_id").
+    where("people_secretsantas.person_id is null" \
+      " and partner.partner_id is null and santa.santa_id is null" \
+      " and previous.previous_santa_id is null ")
   }
 end
