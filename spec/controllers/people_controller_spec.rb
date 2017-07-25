40c529b9052229e467ad89f0592d0c9ee1aad7ea
Roy <roybleas@ozemail.com.au>
2017-07-25 10:11:31 +1000

    Correct the description title in controller spec and create the test to match the title

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -10,9 +10,15 @@ RSpec.describe PeopleController, type: :controller do
   end
 
   describe "GET #show" do
-    it "returns http success" do
+    it "returns http not found" do
       get :show, id: 1
       expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns http success" do
+      ps = FactoryGirl.create(:people_secretsanta)
+      get :show, id: ps.id
+      expect(response).to have_http_status(:success)
     end
   end
 
