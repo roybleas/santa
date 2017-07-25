90a2f6d00735657334af165bbcd80cc5f1d1c1b5
Roy <roybleas@ozemail.com.au>
2017-07-25 10:45:01 +1000

    a flash message is only deleted after a redirect so set to flash.now

--- app/controllers/concerns/show_message.rb
+++ app/controllers/concerns/show_message.rb
@@ -3,7 +3,7 @@ module ShowMessage
 
   def when_missing_santa_ids_show_message(this_year)
     if PeopleSecretsantas.by_year(this_year).where("santa_id is null").count > 0
-      flash[:warning] = "Warning: Participants have not all been allocated to be a Secret Santa." 
+      flash.now[:warning] = "Warning: Participants have not all been allocated to be a Secret Santa."
     end
   end
 
